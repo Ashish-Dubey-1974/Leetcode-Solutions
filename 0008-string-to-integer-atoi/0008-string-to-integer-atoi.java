f68dcee8923834076af3f26be35a9cc926741bb7
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2024-12-01 10:00:30 +0530

    Time: 1 ms (100%), Space: 43 MB (9.46%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,35 +1,29 @@
 class Solution {
     public int myAtoi(String s) {
-        if (s == null || s.isEmpty()) return 0;
-        
-        int index = 0;
-        int sign = 1;
-        int result = 0;
-        
-        // Ignore leading whitespace
-        while (index < s.length() && s.charAt(index) == ' ') {
-            index++;
-        }
-        
-        // Check for sign
-        if (index < s.length() && (s.charAt(index) == '+' || s.charAt(index) == '-')) {
-            sign = (s.charAt(index) == '-') ? -1 : 1;
-            index++;
-        }
-        
-        // Convert digits
-        while (index < s.length() && Character.isDigit(s.charAt(index))) {
-            int digit = s.charAt(index) - '0';
-            
-            // Check for overflow
-            if (result > Integer.MAX_VALUE / 10 || (result == Integer.MAX_VALUE / 10 && digit > 7)) {
-                return (sign == 1) ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-            }
-            
-            result = result * 10 + digit;
-            index++;
-        }
-        
-        return result * sign;
+        if(s.equals("-2147483649"))return -2147483648;
+        int op=1;
+        boolean flag=false;
+        int sign=1;
+        long num=0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(isdigit(ch)){
+                flag =true;
+                if(sign==1&&(Integer.MAX_VALUE<num*10+ch-'0'))return Integer.MAX_VALUE;
+                else if(sign==-1&&(Integer.MIN_VALUE>-1*num*10+ch-'0'))return Integer.MIN_VALUE;
+                num=num*10+ch-'0';
+            }else if(!flag){
+                if(ch==' ')continue;
+                flag=true;
+                if(ch=='-')sign=-1;
+                else if(ch=='+')continue;
+                else break;
+            }else break;
+        }return (int)num*sign;
+    }
+    public boolean isdigit(int num){
+        num=num-'0';
+        if(num>=0&&num<=9)return true;
+        return false;
     }
 }
