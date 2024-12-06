8726f296ca57dab4f22a5fc56ef2da7c374be277
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2024-12-06 20:13:02 +0530

    Time: 13 ms (86.01%), Space: 48.2 MB (11.39%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,45 @@
+class Solution {
+    class pair{
+        int r;
+        int c;
+        int d=0;
+        pair(int r,int c,int d){
+            this.r=r;
+            this.c=c;
+            this.d=d;
+        }
+    }
+    public int[][] updateMatrix(int[][] mat) {
+        int n=mat.length;
+        int m=mat[0].length;
+        int[][] M = new int[n][m];
+        boolean[][] vis = new boolean[n][m];
+        Queue<pair> q = new LinkedList<>();
+        // Copy data into new Matrix
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(mat[i][j]==0){
+                    q.add(new pair(i,j,0));
+                    vis[i][j]=true;
+            }
+        }}
+        int[] rn = {0,0,1,-1};
+        int[] cn = {1,-1,0,0};
+        while(!q.isEmpty()){
+            pair p = q.remove();
+            for(int i=0;i<4;i++){
+                int row=p.r+rn[i];
+                int col=p.c+cn[i];
+                if(row>=0&&row<n&&col>=0&&col<m&&!vis[row][col]){
+                    if(mat[row][col]==1){
+                        q.add(new pair(row,col,p.d+1));
+                        M[row][col]=p.d+1;
+                    }
+                    vis[row][col]=true;
+                }
+            }
+        }return M;
+
+    }
+
+}
