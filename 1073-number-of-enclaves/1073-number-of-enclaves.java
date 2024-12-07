896c7065f1db2a28497e9c236a2f2e656b84607b
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2024-12-07 11:30:44 +0530

    Time: 14 ms (9.15%), Space: 55.1 MB (66.11%) - LeetHub

---
+++ 1073-number-of-enclaves/1073-number-of-enclaves.java
@@ -0,0 +1,45 @@
+class Solution {
+    class pair{
+        int r;
+        int c;
+        pair(int r,int c){
+            this.r=r;
+            this.c=c;
+        }
+    }
+    public int numEnclaves(int[][] board) {
+        int cnt=0;
+        int m=board.length;
+        int n=board[0].length;
+        boolean[][] vis = new boolean[m][n];
+        Stack<pair> st = new Stack<>();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==1&& (i==0||j==0||i==m-1||j==n-1)){
+                    st.push(new pair(i,j));
+                    vis[i][j]=true;
+                }
+            }
+        }
+        while(!st.isEmpty()){
+            pair p=st.pop();
+            int[]ra={0,0,1,-1};
+            int[]ca={1,-1,0,0};
+            for(int i=0;i<4;i++){
+                int rn = ra[i]+p.r;
+                int cn = ca[i]+p.c;
+                if(rn>=0&&rn<m&&cn>=0&&cn<n&&board[rn][cn]==1&&!vis[rn][cn]){
+                    st.add(new pair(rn,cn));
+                    vis[rn][cn]=true;
+                }
+            }
+        }
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==1&& !vis[i][j]){
+                    cnt++;
+                }
+            }
+        }return cnt;
+    }
+}
