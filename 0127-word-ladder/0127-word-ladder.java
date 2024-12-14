3b93887c61b548c9cbb8bfa680e8fd6610f7268a
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2024-12-14 22:11:39 +0530

    Time: 216 ms (23.44%), Space: 46.9 MB (49.98%) - LeetHub

---
+++ 0127-word-ladder/0127-word-ladder.java
@@ -0,0 +1,35 @@
+class Solution {
+    class pair{
+        String s;
+        int c;
+        pair(String s,int c){
+            this.s=s;this.c=c;
+        }
+    }
+    public int ladderLength(String begin, String end, List<String> wordList) {
+        HashSet<String> map = new HashSet<>();
+        Queue<pair> q = new LinkedList<>();
+        int cnt=0;
+        q.add(new pair(begin,cnt));
+        for(int i=0;i<wordList.size();i++)map.add(wordList.get(i));
+        if(map.contains(begin))map.remove(begin);
+        if(map.contains(end)){
+            while(!q.isEmpty()){
+                pair p = q.remove();
+                for(int i=0;i<p.s.length();i++){
+                    for(char c='a';c<='z';c++){
+                        String m = p.s.substring(0,i)+c+p.s.substring(i+1);
+                        if(m.equals(end))return p.c+2;
+                        else if(map.contains(m)){
+                            // System.out.print(m+" "+p.c+"  ");
+                            q.add(new pair(m,p.c+1));
+                            map.remove(m);
+                        }
+                    }
+                }
+            }
+            return cnt;
+        }
+        else return cnt;
+    }
+}
