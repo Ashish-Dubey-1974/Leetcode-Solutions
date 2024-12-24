dcffc79cd99239f94ed1aa4ff8dc3d8e98517cc0
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2024-12-24 21:59:12 +0530

    Time: 62 ms (39.27%), Space: 52.1 MB (23.78%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,21 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let original = init; 
+    let initial = init;  
+    return {
+        increment() {
+            return ++original;
+        },
+        reset() {
+            original = initial; 
+            return original;
+        },
+        decrement() {
+            return --original; 
+        }
+    };
+};
+
