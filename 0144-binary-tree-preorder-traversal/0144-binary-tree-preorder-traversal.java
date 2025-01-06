ccc74d8236b20b0474c935214e7825f6750edff3
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2025-01-06 15:18:49 +0530

    Time: 0 ms (100%), Space: 41.7 MB (53.19%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -1,49 +1,13 @@
 class Solution {
+    List<Integer> list = new ArrayList<>();
     public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
         if(root==null)return list;
-        Stack<TreeNode> st = new Stack<>();
-        st.push(root);
-        while(!st.isEmpty()){
-            TreeNode curr = st.pop();
-            list.add(curr.val);
-            if(curr.right!=null)st.push(curr.right);
-            if(curr.left!=null)st.push(curr.left);
-        }
+        list.add(root.val);
+        preorderTraversal(root.left);
+        preorderTraversal(root.right);
         return list;
     }
 }
-
-
-
-
-
-
-
-
-
-
-// public List<Integer> preorderTraversal(TreeNode root) {
-//         List<Integer> list = new ArrayList<>();
-//         if (root == null) return list;
-
-//         Stack<TreeNode> stack = new Stack<>();
-//         stack.push(root);
-
-//         while (!stack.isEmpty()) {
-//             TreeNode node = stack.pop();
-//             list.add(node.val); // Visit the root
-
-//             // Push right child first so that left child is processed next
-//             if (node.right != null) stack.push(node.right);
-//             if (node.left != null) stack.push(node.left);
-//         }
-
-//         return list;
-//     }}
-
-
-
 
 
 
