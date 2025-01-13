9cd68cb133bc2fd4a6fbe461c091f30433e8775f
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2025-01-13 14:54:33 +0530

    Time: 55 ms (43.05%), Space: 46.2 MB (66.89%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -1,6 +1,7 @@
 class Solution {
     public int minimumLength(String s) {
         StringBuilder sb = new StringBuilder(s);
+        int sum=sb.length();
         int[] arr1 = new int[26];
         int[] arr2 = new int[26];
         Arrays.fill(arr1,-1);
@@ -11,18 +12,16 @@ class Solution {
             if(arr1[idx]==-1)arr1[idx]=i;
             else if(arr2[idx]==-1)arr2[idx]=i;
             else {
+                sum-=2;
                 int m1 = arr1[idx];
                 int m2 = arr2[idx];
                 sb.setCharAt(m1,'*');
                 arr1[idx] = m2;
                 sb.setCharAt(i,'*');
                 arr2[idx]=-1;
-                // System.out.println(sb+" "+i+" m1 "+map1+" m2 "+map2);
             }   
              
         }
-        int sum=0;
-        for(int i=0;i<sb.length();i++)if(sb.charAt(i)!='*')sum++;
         return sum;
     }
 }
