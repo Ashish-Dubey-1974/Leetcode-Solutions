7befef9f80d94dfc193e4e68fde2909bfbb75a15
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2025-01-14 08:51:52 +0530

    Time: 9 ms (92%), Space: 46.7 MB (25.33%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.java
@@ -1,26 +1,12 @@
 class Solution {
     public int minimumLength(String s) {
-        StringBuilder sb = new StringBuilder(s);
-        int sum=sb.length();
+        int sum=0;
         int[] arr1 = new int[26];
-        int[] arr2 = new int[26];
-        Arrays.fill(arr1,-1);
-        Arrays.fill(arr2,-1);
-        for(int i=0;i<sb.length();i++){
-            char ch = sb.charAt(i);
-            int idx = ch-'a';
-            if(arr1[idx]==-1)arr1[idx]=i;
-            else if(arr2[idx]==-1)arr2[idx]=i;
-            else {
-                sum-=2;
-                int m1 = arr1[idx];
-                int m2 = arr2[idx];
-                sb.setCharAt(m1,'*');
-                arr1[idx] = m2;
-                sb.setCharAt(i,'*');
-                arr2[idx]=-1;
-            }   
-             
+        for(int i=0;i<s.length();i++)arr1[s.charAt(i)-'a']++;
+        for(int i:arr1){
+            if(i==0)continue;
+            else if(i%2==0)sum+=2;
+            else sum++;
         }
         return sum;
     }
