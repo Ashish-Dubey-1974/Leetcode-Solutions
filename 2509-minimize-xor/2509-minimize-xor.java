9d6d714ad117fecfaab0e6505fac04f8cdc7f7c8
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2025-01-15 11:12:59 +0530

    Time: 0 ms (100%), Space: 40.6 MB (63.21%) - LeetHub

--- 2509-minimize-xor/2509-minimize-xor.java
+++ 2509-minimize-xor/2509-minimize-xor.java
@@ -1,12 +1,11 @@
 class Solution {
   public int minimizeXor(int num1, int num2) {
-    int kMaxBit = 30;
-    int bits = Integer.bitCount(num2);
-    if (Integer.bitCount(num1) == bits)
+    final int kMaxBit = 30;
+    int bits = counter(num2);
+    if (counter(num1) == bits)
       return num1;
 
     int ans = 0;
-
     for (int i = kMaxBit - 1; i >= 0; --i)
       if ((num1 >> i & 1) == 1) {
         ans |= 1 << i;
@@ -22,4 +21,11 @@ class Solution {
 
     return ans;
   }
+  int counter(int val){
+        int cnt=0;
+        while(val>0){
+            val=val&(val-1);
+            cnt++;
+        }return cnt;
+    }
 }
