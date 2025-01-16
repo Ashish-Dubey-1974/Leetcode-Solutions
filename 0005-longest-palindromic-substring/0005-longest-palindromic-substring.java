c1112c24141c036d6d894b3faba2511e70a0323b
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2025-01-16 10:53:07 +0530

    Time: 15 ms (86.41%), Space: 42.3 MB (75.03%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,6 +1,6 @@
 class Solution {
     public String longestPalindrome(String s) {
-        if (s == null || s.length() == 0) return "";
+        if (s.length() <2) return s;
         
         int start = 0;
         int end = 0;
@@ -14,7 +14,7 @@ class Solution {
                 start = i - (len - 1) / 2;
                 end = i + len / 2;
             }
-        }
+        } 
         
         return s.substring(start, end + 1);
         
