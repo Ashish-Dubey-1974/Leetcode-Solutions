128bf7bc1dab71dfd524f4c7fb0d09613ae5c7eb
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2025-05-28 22:13:57 +0530

    Time: 1 ms (100%), Space: 42.5 MB (66.27%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,30 +1,24 @@
+
 class Solution {
     public int myAtoi(String s) {
-        int op=1;
-        boolean flag=false;
-        int sign=1;
-        long num=0;
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            if(isdigit(ch)){
-                flag =true;
-                if (sign == 1 && num > (Integer.MAX_VALUE - (ch - '0')) / 10) {
-                    return Integer.MAX_VALUE;
-                } else if (sign == -1 && -num < (Integer.MIN_VALUE + (ch - '0')) / 10) {
-                    return Integer.MIN_VALUE;
-                } num=num*10+ch-'0';
-            }else if(!flag){
-                if(ch==' ')continue;
-                flag=true;
-                if(ch=='-')sign=-1;
-                else if(ch=='+')continue;
-                else break;
-            }else break;
-        }return (int)num*sign;
+        s=s.trim();
+        return (int)helper(s,0,0,1,false);
     }
-    public boolean isdigit(int num){
-        num=num-'0';
-        if(num>=0&&num<=9)return true;
-        return false;
+    long helper(String s,long num,int idx,int sign,boolean flag){
+        if(idx>=s.length())return num*sign;
+        if(Character.isDigit(s.charAt(idx))){
+            flag=true;
+            int val = s.charAt(idx)-'0';
+            long check = num*10+val;
+            if(sign==1&&check>Integer.MAX_VALUE)return Integer.MAX_VALUE;
+            if(sign==-1&&-check<Integer.MIN_VALUE)return Integer.MIN_VALUE;
+            num=num*10+val;
+        }else if(!flag){
+            flag=true;
+            if(s.charAt(idx)=='-')sign=-1;
+            else if(s.charAt(idx)=='+')sign=1;
+            else return num*sign;
+        }else return num*sign;
+        return helper(s,num,idx+1,sign,flag);
     }
 }
