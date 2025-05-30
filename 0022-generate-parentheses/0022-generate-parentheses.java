a2cad5bf1153af388400a1a7516dc3413da78a86
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2025-05-30 08:00:31 +0530

    Time: 2 ms (46.7%), Space: 42.9 MB (88.85%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,26 +1,27 @@
 class Solution {
     List<String> ans = new ArrayList<>();
+    
     public List<String> generateParenthesis(int n) {
-        generate("",0,n*2);
+        generate("",0,n,0,0);
         return ans;
     }
-    void generate(String s,int len,int n){
-        if(len==n){
-            if(valid(s))ans.add(s);
+    void generate(String s,int len,int n,int o,int c){
+        if(len==n*2){
+            ans.add(s);
             return;
         }
-        generate(s+"(",1+len,n);
-        generate(s+")",1+len,n);
+        if(o<n)generate(s+"(",1+len,n,o+1,c);
+        if(o>c)generate(s+")",1+len,n,o,c+1);
     }
-    boolean valid(String s){
-        Stack<Character> st = new Stack<>();
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='(')st.push(s.charAt(i));
-            else{
-                if(st.isEmpty()||st.pop()!='(')return false;
-            }
-        }
-        return st.isEmpty();
-    }
+    // boolean valid(String s){
+    //     Stack<Character> st = new Stack<>();
+    //     for(int i=0;i<s.length();i++){
+    //         if(s.charAt(i)=='(')st.push(s.charAt(i));
+    //         else{
+    //             if(st.isEmpty()||st.pop()!='(')return false;
+    //         }
+    //     }
+    //     return st.isEmpty();
+    // }
 
 }
