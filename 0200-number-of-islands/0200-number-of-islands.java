29199cb1b89bcecdb9c576a25bd762bf44e0e32e
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2025-06-13 16:55:35 +0530

    Time: 9 ms (12.07%), Space: 49.3 MB (79.73%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -3,35 +3,39 @@ class Solution {
         int r;
         int c;
         pair(int r,int c){
-            this.r=r;this.c=c;
+            this.r=r;
+            this.c=c;
         }
     }
     public int numIslands(char[][] grid) {
-        int m=grid.length,n=grid[0].length;
-        int cnt=0;
-        int[] ra = {0,0,1,-1};
-        int[] ca = {1,-1,0,0};
-        boolean[][] vis = new boolean[m][n];
-        // dfs
+        int n = grid.length;
+        int m = grid[0].length;
+        int ans=0;
+        int[] row = {0,0,1,-1};
+        int[] col = {1,-1,0,0};
         Stack<pair> st = new Stack<>();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]=='1'&&!vis[i][j]){
-                    vis[i][j]=true;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    
+                    grid[i][j]='*';
+                    ans++;
+                    // System.out.print(ans+" ");
                     st.push(new pair(i,j));
                     while(!st.isEmpty()){
                         pair p = st.pop();
                         for(int x=0;x<4;x++){
-                            int r = ra[x]+p.r;
-                            int c = ca[x]+p.c;
-                            if(r>=0&&r<m&&c>=0&&c<n&&grid[r][c]=='1'&&!vis[r][c]){
-                                vis[r][c]=true;
+                            int r = p.r+row[x];
+                            int c = p.c+col[x];
+                            if(r>=0&&r<n&&c>=0&&c<m&&grid[r][c]=='1'){
+                                grid[r][c]='*';
                                 st.push(new pair(r,c));
                             }
                         }
-                    }cnt++;
+                    }
                 }
             }
-        }return cnt;
+        }
+        return ans;
     }
 }
