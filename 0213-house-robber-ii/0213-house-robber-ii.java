34bf6373b5f2b2454627908001d9f69294adac72
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2025-06-26 16:23:46 +0530

    Time: 0 ms (100%), Space: 41.5 MB (18.78%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,19 +1,19 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length==1)return nums[0];
-        int[] dp = new int[nums.length];
+        int n = nums.length;
+        if(n==1)return nums[0];
+        int[] dp = new int[n];
         Arrays.fill(dp,-1);
-        int a = ruby(nums,0,false,dp);
+        int a = ruby(nums,0,n-1,dp);
         Arrays.fill(dp,-1);
-        int b = ruby(nums,1,true,dp);
+        int b = ruby(nums,1,n,dp);
         return Math.max(a,b);
     }
-    int ruby(int[] nums,int idx,boolean flag,int[] dp){
-        if(flag&&idx>=nums.length)return 0;
-        if(!flag&&idx>=nums.length-1)return 0;
+    int ruby(int[] nums,int idx,int end,int[] dp){
+        if(idx>=end)return 0;
         if(dp[idx]!=-1)return dp[idx];
-        int rob = nums[idx]+ruby(nums,idx+2,flag,dp);
-        int skip = ruby(nums,idx+1,flag,dp);
+        int rob = nums[idx]+ruby(nums,idx+2,end,dp);
+        int skip = ruby(nums,idx+1,end,dp);
         dp[idx] = Math.max(rob,skip);
         return dp[idx];
     }
