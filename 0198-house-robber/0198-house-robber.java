55709a9de9d8391d11bf746f8ec687a73bc9eb7a
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2025-06-26 15:46:01 +0530

    Time: 0 ms (100%), Space: 41.1 MB (47.21%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,24 +1,17 @@
 class Solution {
-    public int rob(int[] num) {
-        int n=num.length;
-        int[] dp = new int[num.length];
+    int max = 0;
+    int[] dp ;
+    public int rob(int[] nums) {
+        dp = new int[nums.length];
         Arrays.fill(dp,-1);
-        return find(0,num,dp);
-        // if(num==null||n==0)return 0;
-        // if(n==1)return num[0];
-        // if(n==2)return Math.max(num[0],num[1]);
-        // int[] dp = new int[n];
-        // dp[0]=num[0];
-        // dp[1] = Math.max(num[1],num[0]);
-        // for(int i=2;i<n;i++)dp[i]=Math.max(num[i]+dp[i-2],dp[i-1]);
-        // return dp[n-1];
+        return helper(nums,0);
     }
-    static int find(int i,int[] nums,int[] dp){
-        if(i>=nums.length)return 0;
-        if(dp[i]!=-1)return dp[i];
-        int rob = nums[i]+find(i+2,nums,dp);
-        int drob = find(i+1,nums,dp);
-        dp[i]=Integer.max(rob,drob);
-        return dp[i];
-    } 
+    int helper(int[] nums,int idx){
+        if(idx>=nums.length) return 0;
+        if(dp[idx]!=-1)return dp[idx];
+        int rob =nums[idx]+helper(nums,idx+2);
+        int skip =helper(nums,idx+1);
+        dp[idx] = Math.max(rob,skip);
+        return dp[idx];
+    }
 }
