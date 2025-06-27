94f9357f15b69141ffb0beacc0911ebebf376deb
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2025-06-27 11:13:37 +0530

    Time: 0 ms (100%), Space: 40.9 MB (10.14%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,11 +1,15 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] ar = new int[m][n];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0||j==0)ar[i][j]=1;
-                else ar[i][j]=ar[i-1][j]+ar[i][j-1];
-            }
-        }return ar[m-1][n-1];
+        int[][] dp = new int[m][n];
+        for(int[] row : dp)Arrays.fill(row,-1);
+        return count(m,n,0,0,dp);
+    }
+    int count(int row,int col,int r,int c,int[][] dp){
+        if(row-1==r&&col-1==c)return 1;
+        if(row<=r || col<=c )return 0;
+        if(dp[r][c]!=-1)return dp[r][c];
+        int down = count(row,col,r+1,c,dp);
+        int left = count(row,col,r,c+1,dp);
+        return dp[r][c]=down+left;
     }
 }
