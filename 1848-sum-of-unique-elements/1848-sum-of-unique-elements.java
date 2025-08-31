27e79ba11ffe5bffeb8341fa8b44b6a86670df19
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2025-08-31 18:06:08 +0530

    Time: 0 ms (100%), Space: 41.2 MB (72.62%) - LeetHub

---
+++ 1848-sum-of-unique-elements/1848-sum-of-unique-elements.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int sumOfUnique(int[] nums) {
+        int[] freq = new int[101];
+        for(int i=0;i<nums.length;i++)freq[nums[i]]++;
+        int ans=0;
+        for(int i=1;i<=100;i++)if(freq[i]==1)ans+=i;
+        return ans;
+    }
+}
