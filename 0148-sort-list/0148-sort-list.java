d9b413c4c5976f49021a1283ebd30272415ea046
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2025-10-04 17:00:13 +0530

    Time: 9 ms (96.38%), Space: 57.2 MB (23.72%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,16 +1,35 @@
 class Solution {
     public ListNode sortList(ListNode head) {
-        PriorityQueue<Integer> q = new PriorityQueue<>();
-        ListNode temp = head;
-        while(temp!=null){
-            q.add(temp.val);
-            temp=temp.next;
-        }
-        temp = head;
-        while(temp!=null){
-            temp.val=q.poll();
-            temp=temp.next;
-        }return head;
+        if(head==null||head.next==null)return head;
+        ListNode mid = getMid(head);
+        ListNode right = mid.next;
+        mid.next = null;
+        ListNode l = sortList(head);
+        ListNode r = sortList(right);
+        return merge(l,r);
 
+    }
+    ListNode getMid(ListNode head){
+        ListNode slow=head,fast=head.next;
+        while(fast!=null&&fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }return slow;
+    }
+    ListNode merge(ListNode l,ListNode r){
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while(l!=null &&r!=null){
+        if(l.val<r.val){
+            tail.next=l;
+            l = l.next;
+        }else{
+            tail.next=r;
+            r=r.next;
+        }tail=tail.next;}
+
+        if(l!=null)tail.next=l;
+        if(r!=null)tail.next=r;
+        return dummy.next;
     }
 }
