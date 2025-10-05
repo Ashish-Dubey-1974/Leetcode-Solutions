c77d2452f86fb287f2272ba26b99bc8bb4f48a35
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2025-10-05 16:06:53 +0530

    Time: 9 ms (96.37%), Space: 57 MB (40.25%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,37 +1,34 @@
 class Solution {
-    public static  ListNode sortList(ListNode head) {
-        if(head==null||head.next==null)return head;
+    public ListNode sortList(ListNode head) {
+        if(head==null || head.next==null)return head;
         ListNode mid = getMid(head);
         ListNode right = mid.next;
-        mid.next = null;
-        ListNode l = sortList(head);
-        ListNode r = sortList(right);
-        return merge(l,r);
-
+        mid.next=null;
+        ListNode Ls = sortList(head);
+        ListNode Rs = sortList(right);
+        return merge(Ls,Rs);
     }
-    static ListNode getMid(ListNode head){
-        ListNode slow=head,fast=head.next;
-        while(fast!=null&&fast.next!=null){
-            slow = slow.next;
-            fast = fast.next.next;
-        }return slow;
-    }
-    static ListNode merge(ListNode l,ListNode r){
+    ListNode merge(ListNode l,ListNode r){
         ListNode dummy = new ListNode(0);
-        ListNode tail = dummy;
-        while(l!=null &&r!=null){
-        if(l.val<r.val){
-            tail.next=l;
-            l = l.next;
-        }else{
-            tail.next=r;
-            r=r.next;
-        }
-        tail=tail.next;
-        }
-
-        if(l!=null)tail.next=l;
+        ListNode tail=dummy;
+        while(l!=null && r!=null){
+            if(l.val<r.val){
+                tail.next = l;
+                l = l.next;
+            }else{
+                tail.next = r;
+                r = r.next;
+            }tail = tail.next;
+        }if(l!=null)tail.next=l;
         if(r!=null)tail.next=r;
         return dummy.next;
+
+    }
+    ListNode getMid(ListNode head){
+        ListNode slow=head,fast=head.next;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }return slow;
     }
 }
