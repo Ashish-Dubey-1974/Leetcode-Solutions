9cb7d498b024e983a202d98099c85eb2057144a6
Ashish Dubey <161051825+Ashish-Dubey-1974@users.noreply.github.com>
2025-10-07 10:41:53 +0530

    Time: 13 ms (40.28%), Space: 44.6 MB (38.58%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.java
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.java
@@ -1,24 +1,23 @@
 class Solution {
     public int swimInWater(int[][] grid) {
         int n = grid.length;
-        boolean[][] visited = new boolean[n][n];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[0] - b[0]);
-        pq.offer(new int[]{grid[0][0], 0, 0}); // {time, row, col}
-        int[][] dirs = {{1,0},{-1,0},{0,1},{0,-1}};
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int time = cur[0], r = cur[1], c = cur[2];
-            if (r == n - 1 && c == n - 1) return time;
-            if (visited[r][c]) continue;
-            visited[r][c] = true;
-            
-            for (int[] d : dirs) {
-                int nr = r + d[0], nc = c + d[1];
-                if (nr >= 0 && nc >= 0 && nr < n && nc < n && !visited[nr][nc]) {
-                    pq.offer(new int[]{Math.max(time, grid[nr][nc]), nr, nc});
-                }
+        boolean[][] vis = new boolean[n][n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]-b[0]);
+        pq.add(new int[]{grid[0][0],0,0});
+        int[] row = {0,0,1,-1};
+        int[] col = {1,-1,0,0};
+        while(!pq.isEmpty()){
+            int[] arr = pq.poll();
+            int time = arr[0],r = arr[1],c = arr[2];
+            if(r==n-1 && c==n-1)return time;
+            if(vis[r][c])continue;
+            vis[r][c]=true;
+            for(int i=0;i<4;i++){
+                int nr = r+row[i];
+                int nc = c+col[i];
+                if(nr>=0 && nc>=0 && nr<n && nc<n && !vis[nr][nc])
+                pq.add(new int[]{Math.max(grid[nr][nc],time),nr,nc});
             }
-        }
-        return -1;
+        }return 0;
     }
 }
